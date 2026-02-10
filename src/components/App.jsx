cead3c5468f07786705fedf999b21adfd00e7c3c
malinachitic24-star <malinachitic24@gmail.com>
2026-02-10 22:01:51 +0100

    Creación de repositorio

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+//import { useState } from 'react'
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
