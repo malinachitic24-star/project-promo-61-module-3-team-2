514483ecd3a6fcc9c539c14342c8a4ed046379a6
COELHO DO AMARAL <soraya.coelho@t-systems.com>
2026-02-14 09:59:27 +0100

    Archivo desde main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
-//import { useState } from 'react'
-import "../styles/App.scss";
-import Header from "./Header";
-import Landing from "./Landing/Landing";
 import { Routes, Route } from "react-router-dom";
+import Landing from "./Landing/Landing.jsx";
 
 function App() {
   return (
-    <div className="body">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Landing />}></Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from "react-router-dom";
 import App from "./components/App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
