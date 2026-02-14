fc0948dd22c7eb71b5be7dfd968bd1d4151015c4
Nidia23 <nidia_ibz_23hotmail.com>
2026-02-14 13:57:46 +0100

    cards+aboutus

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'
 import "../styles/App.scss";
+import AboutUs from "./AboutUs";
 import Header from "./Header";
 import Landing from "./Landing/Landing";
 import { Routes, Route } from "react-router-dom";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="body">
       <Header />
+      <AboutUs></AboutUs>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
       </Routes>
