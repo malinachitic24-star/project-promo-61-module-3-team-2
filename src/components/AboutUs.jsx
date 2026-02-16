40a07c6ea47c9b5674ea384a7ddf429b55334cfc
Nidia23 <nidia_ibz_23hotmail.com>
2026-02-16 21:40:47 +0100

    cards.scss

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,24 +5,24 @@ import AstroLogo from "../images/avatar.webp";
 function AboutUs (){
     const company= [
         {
-            imagen: {AstroLogo},
+            imagen: AstroLogo,
             nombre:"Nidia",
             tech:"Tech",
         },
         {
-            imagen: "",
-            nombre:"",
-            tech:"",
+            imagen: AstroLogo,
+            nombre:"Cristina",
+            tech:"Tech",
         },
         {
-            imagen: "",
-            nombre:"",
-            tech:"",
+            imagen: AstroLogo,
+            nombre:"Soraya",
+            tech:"Tech",
         },
         {
-            imagen: "",
-            nombre:"",
-            tech:"",
+            imagen: AstroLogo,
+            nombre:"Malina",
+            tech:"Tech",
         }
 
     ];
@@ -33,14 +33,14 @@ function AboutUs (){
         <section>
             <div className="container-about-us">
                 <div className="about-us-info">
-                <h1 className="about-us-title">Women in Tech Space</h1>
-                <p className="about-us-text">
-                    <b>¿Y si las oportunidades y la igualdad, como el espacio, no tuvieran límites?</b><br></br>
-                    Esa fue la pregunta que potenció la idea revolucionaria de Women in Tech Space. Un espacio donde la mujer es la protagonista de sus sueños, sus ideas y sus proyectos, compartiéndolos en una comunidad de oportunidades e igualdad ante el monopolio masculino del mundo tech.
-                    Caminamos de la mano de una de las más inspiradoras empresas, Adalab, quienes lideran la formación y el acompañamiento de las inconformistas, las valientes, las rebeldes y las decididas a tener una vida mejor sin importar su procedencia.
+                    <h1 className="about-us-title">Women in Tech Space</h1>
+                    <p className="about-us-text">
+                        <b>¿Y si las oportunidades y la igualdad, como el espacio, no tuvieran límites?</b><br></br>
+                        Esa fue la pregunta que potenció la idea revolucionaria de Women in Tech Space. Un espacio donde la mujer es la protagonista de sus sueños, sus ideas y sus proyectos, compartiéndolos en una comunidad de oportunidades e igualdad ante el monopolio masculino del mundo tech.
+                        Caminamos de la mano de una de las más inspiradoras empresas, Adalab, quienes lideran la formación y el acompañamiento de las inconformistas, las valientes, las rebeldes y las decididas a tener una vida mejor sin importar su procedencia.
                     
-                    Teniendo todas algo en común: somos las valientes preparadas para brillar.    
-                </p>
+                        Teniendo todas algo en común: somos las valientes preparadas para brillar.    
+                    </p>
                 </div>
                 <div className="about-us-container-card">
                     {company.map((persona,index)=>(
--- src/components/propts/Cards.jsx
+++ src/components/propts/Cards.jsx
@@ -3,18 +3,17 @@ import "../../styles/LandingSyles/Cards.scss";
 const Cards= ({imagen,proyecto,nombre,tech})=> {
     return(
         <div className="card_container">
-            <header className="card_header">
+            <div className="card_container_head">
+                <h3 className="card_title">{nombre}</h3>
                 <img
                     className="card_project-img"
                     src={imagen}
                     alt={proyecto}
                 />
-            </header>
-
-            <div className="card_body">
-                <h3 className="card_title">{nombre}</h3>
-                <p className="card_"></p>
-                <p className="card_tech">{tech}</p>
+                <div className="card_container_info">
+                    <p className="card_"></p>
+                    <p className="card_tech">{tech}</p>
+                </div>
             </div>
         </div>
     )
